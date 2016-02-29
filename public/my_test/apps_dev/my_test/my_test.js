6d03181c011d275885774492639a6f0aadb4a954
NoxKeini <vlatolkach@mail.ru>
2016-02-29 15:46:57 +0300

    Убрана проблема с ошибкой при ненахождении объекта Добавлена проверка моделей новых пользователей

--- public/my_test/apps_dev/my_test/my_test.js
+++ public/my_test/apps_dev/my_test/my_test.js
@@ -20,6 +20,7 @@ var DEBUG = (m_ver.type() === "DEBUG");
 var APP_ASSETS_PATH = m_cfg.get_std_assets_path() + "my_test/"; //в дальнейшем путь был исправлен мной в связи с перемещением основного файла приложения в папку public
 
 var USER_NAME;
+//var loaded_model_list = ['my_cube'];
 /**
  * export the method to initialize the app (called at the bottom of this file)
  */
@@ -52,7 +53,6 @@ function init_cb(canvas_elem, success) {
 function load() {
     APP_ASSETS_PATH = '/my_test/deploy/assets/my_test/'
     m_data.load(APP_ASSETS_PATH + "my_test.json", load_cb);   
-    //console.log(APP_ASSETS_PATH + "my_test.json");
 }
 
 /**
@@ -66,34 +66,8 @@ function load_cb(data_id, success) {
     }
 
     m_app.enable_controls();
-    //m_app.enable_object_controls(m_scenes.get_object_by_name("Cube"));
-    //m_app.enable_camera_controls();
-
-/*
-    //Передача координат объекта при перемещении в сокет
-    var cube = m_scenes.get_object_by_name("Cube");
-    var sensor_motion_cube = m_controls.create_motion_sensor(cube);
-    // array of the sensors
-    var impact_sens_array = [sensor_motion_cube];
-    // manifold logic function
-    var impact_sens_logic = function(s) {
-        return (s[0])
-    };
-    // callback
-    var impact_cb = function(obj, manifold_id, pulse) {
-        var cord_cube = m_transform.get_translation(cube);
-        //console.log(cord_cube);
-        socket.emit('move', cord_cube);
-    }
-    //создание sensor_manifold   
-    m_controls.create_sensor_manifold(cube, "IMPACT", m_controls.CT_CONTINUOUS, impact_sens_array, impact_sens_logic, impact_cb);
-*/
-	
-	
-
+    //m_app.enable_camera_controls(); //контроль камеры
     var socket = io.connect();
-
-
 
     socket.on('check', function () {
         var avatar = m_scenes.get_object_by_name(USER_NAME,m_scenes.DATA_ID_ALL);
@@ -105,119 +79,133 @@ function load_cb(data_id, success) {
     });
 
     socket.on('user_update', function () {
-        var data = arguments[0].data;
+        var name = arguments[0].data.data.name;
+        var pos = arguments[0].data.data.pos;       
         var socket_id = arguments[0].socket_id;
-        //console.log(data, socket_id);
         var avatar_source;
         var model_path = '/my_test/deploy/assets/my_test/'; //путь к папке с моделью
         m_data.load(model_path + "my_cube.json", user_update); //загрузка модели с функцией обратного вызова 
         function user_update(data_id, success){ //функция обновления участников
             avatar_source = m_scenes.get_object_by_name("Cube",data_id); //выделение объекта по id
             m_scenes.hide_object(avatar_source);
-            user_update1(data_id);          
-        }
-        function user_update1(data_id){
-            var name = data.name;
-            var pos = data.pos;
             var avatar_copy = m_objects.copy(avatar_source, name);
             avatar_copy.socket_id = socket_id;
             m_scenes.append_object(avatar_copy, 'Scene');
-            m_scenes.show_object(avatar_copy);
             m_transform.set_translation(avatar_copy,pos[0],pos[1],pos[2]);
-        } 
+            m_scenes.show_object(avatar_copy);                       
+        }
     });
 
     socket.on('connect', function () { //реакция на событие коннекта пользователя
     	var socket_id = '/#'+socket.id;
-    	//console.log(socket_id);
         var model_path = '/my_test/deploy/assets/my_test/'; //путь к папке с моделью
-        m_data.load(model_path + "my_cube.json", translation_avatar); //загрузка модели с функцией обратного вызова перемещения 
-
+        var model_name = "my_cube";
+        m_data.load(model_path + model_name +".json", translation_avatar); //загрузка модели с функцией обратного вызова перемещения 
 	    function translation_avatar(){ //функция перемещения объекта
-	        var avatar = m_scenes.get_object_by_name("Cube",m_scenes.DATA_ID_ALL); //выделение объекта по id     
+	        var avatar = m_scenes.get_object_by_name("Cube",m_scenes.DATA_ID_ALL); //выделение объекта по id
+            //console.log(m_scenes.get_object_data_id());     
 	        var name = "user" + Math.random(); //создание нового имени объекта
 	        var avatar_copy = m_objects.copy(avatar, name);
+            m_scenes.hide_object(avatar);            
 	        avatar_copy.socket_id = socket_id;
-	        //console.log(avatar_copy.socket_id);
+            avatar_copy.model_name = model_name;
 	        m_scenes.append_object(avatar_copy, 'Scene');
-	        var type = 'Mesh';
-	        m_scenes.hide_object(avatar);
 	        USER_NAME = name; //присвоение имени пользователя
 	        var pos = [Math.random()*3,Math.random()*3,Math.random()*3]; //создание случайного вектора перемещения
 	        m_transform.set_translation(avatar_copy,pos[0],pos[1],pos[2]); // присвоение позиции объекта случайному вектору
-	        socket.emit('new_user',{type,name,pos});
-
+	        socket.emit('new_user',{model_name,name,pos});
+            //блок сенсора передвижения
 	        m_app.enable_object_controls(avatar_copy);
 			var sensor_motion_avatar = m_controls.create_motion_sensor(avatar_copy);
-		    // array of the sensors
 		    var impact_sens_array = [sensor_motion_avatar];
-		    // manifold logic function
 		    var impact_sens_logic = function(s) {
 		        return (s[0])
 		    };
-		    // callback
 		    var impact_cb = function(obj, manifold_id, pulse) {
 		        var cord_avatar = m_transform.get_translation(avatar_copy);
-		        //console.log(cord_cube);
 		        socket.emit('move', {'cord':cord_avatar,'name':USER_NAME});
 		    }
-		    //создание sensor_manifold   
 		    m_controls.create_sensor_manifold(avatar_copy, "IMPACT", m_controls.CT_CONTINUOUS, impact_sens_array, impact_sens_logic, impact_cb);	         
 		    } 
     });
 
     socket.on('user_move', function(){
-    	//console.log(arguments[0].data[0].name);
     	var user_name = arguments[0].data[0].name;
     	var new_user_pos = arguments[0].data[0].cord;
-    	var avatar = m_scenes.get_object_by_name(user_name,m_scenes.DATA_ID_ALL);
-     	m_transform.set_translation(avatar,new_user_pos[0],new_user_pos[1],new_user_pos[2]);
+        var obj_list = m_scenes.get_all_objects();
+        for(var i = 0; i < obj_list.length; i++){
+            if (obj_list[i].name == user_name){
+                m_transform.set_translation(obj_list[i],new_user_pos[0],new_user_pos[1],new_user_pos[2]); 
+            }
+        }
     });
 
-    socket.on('add_new_user', function(data){
-        var data = data;        
+    socket.on('add_new_user', function(){
+        var name = arguments[0].data.name;
+        var pos = arguments[0].data.pos;
+        var model_name = arguments[0].data.model_name;               
         var avatar_source;
-        var socket_id = data.socket_id;
+        var socket_id = arguments[0].socket_id;
         var model_path = '/my_test/deploy/assets/my_test/'; //путь к папке с моделью
-        m_data.load(model_path + "my_cube.json", user_update2); //загрузка модели с функцией обратного вызова 
-        function user_update2(data_id, success){ //функция обновления участников
-            avatar_source = m_scenes.get_object_by_name("Cube",data_id); //выделение объекта по id
-            m_scenes.hide_object(avatar_source);
-            user_update3(data_id);          
+        var obj_list = m_scenes.get_all_objects();
+        for (var i = 0; i < obj_list.length; i++){
+            if (obj_list[i].model_name == model_name){
+                copy_obj(obj_list[i],name,socket_id,pos);
+                break;    
+            }
+            if (i == obj_list.length-1){
+                console.log(m_data.load(model_path + "my_cube.json", loaded_cb));//загрузка модели с функцией обратного вызова;
+                function loaded_cb(data_id, success){
+                    load_obj(data_id, success, name, socket_id, pos) 
+                };
+                break;  
+            }
         }
-        function user_update3(data_id){
-            var name = data.data.name;
-            var pos = data.data.pos;
-            var avatar_copy = m_objects.copy(avatar_source, name);
-            avatar_copy.socket_id = socket_id;
-            m_scenes.append_object(avatar_copy, 'Scene');
-            m_scenes.show_object(avatar_copy);
-            m_transform.set_translation(avatar_copy,pos[0],pos[1],pos[2]);
-        }        
     });
+
+    function my_get_obj_by_name(name){  //в идеале заменить на РАБОЧУЮ ф-цию из API b4w
+        var obj_name = name;
+        var obj_list = m_scenes.get_all_objects();
+        for(var i = 0; i < obj_list.length; i++){
+            if (obj_list[i].name == obj_name){
+                m_transform.set_translation(obj_list[i],new_user_pos[0],new_user_pos[1],new_user_pos[2]); 
+            }
+        }
+    }
+
+    function load_obj(data_id, success, name, socket_id, pos){ //добавление объекта
+        console.log(data_id, success, name, socket_id, pos);
+        var avatar_source = m_scenes.get_object_by_name("Cube",data_id); //выделение объекта по id
+        m_scenes.hide_object(avatar_source);
+        var avatar_copy = m_objects.copy(avatar_source, name);
+        avatar_copy.socket_id = socket_id;
+        m_scenes.append_object(avatar_copy, 'Scene');
+        m_transform.set_translation(avatar_copy,pos[0],pos[1],pos[2]);             
+        m_scenes.show_object(avatar_copy);          
+    }
+
+    function copy_obj(obj, name, socket_id, pos){ //копирование объекта
+        var avatar_copy = m_objects.copy(obj, name);
+        avatar_copy.socket_id = socket_id;
+        m_scenes.append_object(avatar_copy, 'Scene');
+        m_transform.set_translation(avatar_copy,pos[0],pos[1],pos[2]);             
+        m_scenes.show_object(avatar_copy);          
+    }  
 
     socket.on('user_disconnect', function () { //реакция на событие дисконнекта пользователя
         var socket_id = arguments[0].socket_id ; //сокет ид пользователя, который отдисконнектился
         var scene_obj_list = m_scenes.get_all_objects(); //получение массива всех объектов сцены
         for(var i = 0; i<scene_obj_list.length; i++){ //перебор объектов сцены
         	if(scene_obj_list[i].socket_id){ //проверка наличия свойства сокет ид
-        		//console.log('есть сокет ид');
-        		//console.log(scene_obj_list[i]);
-        		//console.log('сокет ид аватара' + scene_obj_list[i].socket_id);
-        		//console.log('сокет ид пользователя, который отдисконнектился' + socket_id);
 	        	if(scene_obj_list[i].socket_id == socket_id){ //проверка совпадения сокет ид аватара и пользователя, который отдисконнектился
-	        		//console.log('удаление объекта');
 	        		m_scenes.remove_object(scene_obj_list[i]); // удаление аватара пользователя
 	        	}        		
         	}
         }
-        //console.log(m_scenes.get_all_objects());
     });
 }
 
 });
-
-
 
 // import the app module and start the app by calling the init method
 b4w.require("my_test").init();
--- server.js
+++ server.js
@@ -5,15 +5,7 @@ var favicon = require('serve-favicon');
 var serveStatic = require('serve-static');
 var cookieParser = require('cookie-parser');
 var cookieSession = require('cookie-session');
-//var fs = require('fs');
-//var io = require('socket.io');
-//'/public/my_test/apps_dev/my_test/my_test_dev.html'
-//'/public/my_test/apps_dev/my_test'
-
 var app = connect()
-	/*.use(io.on('connection', function (socket) {
-	  		socket.emit('news', { hello: 'world' });
-	}))*/
 	.use(serveStatic(__dirname + '/public', {'index': ['my_test_dev.html']}))
 	.use(morgan('dev'))
 	.use(favicon(__dirname + '/public/my_favicon.png'))
@@ -23,26 +15,8 @@ var app = connect()
 	  keys: ['key1', 'key2'],
 	  maxAge: 5*60*1000
 	}))	
-
-//http.createServer(app).listen(3000);
-
 var server = http.createServer(app);
-
 var io = require('socket.io')(server);
-
-/*io.on('connection', function (socket) {
-	//socket.emit('connection',{'id':socket.id});
-	socket.on('announce', function(data){ //реакция на событие коннекта нового пользователя
-		//console.log(data);
-		socket.broadcast.emit('new_conn', data); //создание сокет-события рассылки имени и положения нового пользователя всем пользователям	
-	})
-
-	//socket.broadcast.on('announce',{rand_vec});
-	//socket.on('move', function(data){
-		//console.log(data);
-	//})
-})*/
-
 /*Описание работы сокетов
 Приконнекте новый участник запрашивает свойства текущих участников событием check. 
 Участники отвечают на запрос событием check_ansver, передавая свои свойства и вызывая событие user_update, которое отображает 
@@ -51,14 +25,13 @@ var io = require('socket.io')(server);
 io.on('connection', function (socket){
 	socket.broadcast.emit('check',{'socket_id':socket.id});
 	socket.on('check_ansver', function (){
-		io.to(arguments[0].socket_id).emit('user_update',{'data':arguments[0].data, 'socket_id':socket.id});
+		io.to(arguments[0].socket_id).emit('user_update',{'data':arguments[0], 'socket_id':socket.id});
 	});
-	socket.on('new_user', function(data){
-		socket.broadcast.emit('add_new_user',{'data':data, 'socket_id':socket.id});
+	socket.on('new_user', function(){
+		socket.broadcast.emit('add_new_user',{'data':arguments[0], 'socket_id':socket.id});
 	})
 	socket.on('disconnect', function (){
 		socket.broadcast.emit('user_disconnect',{'socket_id':socket.id});
-		//console.log(socket.id);
 	});
 	socket.on('move', function (){
 		socket.broadcast.emit('user_move',{'data':arguments});
